767f8d824c22600df2050e24ed5ab5b91ac0ede3
Andrewschaer <andrewschaer@github.com>
2021-09-02 12:58:01 -0400

    deployable

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const server = require('express')()
+const express = require('express');
+const cors = require('cors');
+// const morgan = require('morgan');
+// const dotenv = require('dotenv');
 
-server.listen(7000, () => {
-    console.log('listening on port 7000')
+const PORT = process.env.PORT || 7000;
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+// server.use(morgan());
+// server.use(dotenv());
+
+server.get('/', (req, res) => {
+    res.send('<h1>WEB 45 ROCKS</h1>')
+})
+
+server.get('/api', (req, res) => {
+    res.json({message: 'WEB 45 ROCKS and IS awesome'})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
